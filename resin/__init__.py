7a0c57dff3e36732d987400b1e432ed80e7a0524
James <james@resin.io>
2015-10-27 11:46:36 +0700

    Implement basic 2FA functionalities: - is_enabled() - is_passed() - challenge()

--- resin/__init__.py
+++ resin/__init__.py
@@ -12,6 +12,7 @@ from .token import Token
 from .logs import Logs
 from .settings import Settings
 from .models import Models
+from .twofactor_auth import TwoFactor_Auth
 
 
 __version__ = '1.1.1'
@@ -33,3 +34,5 @@ class Resin(object):
         self.logs = Logs()
         self.auth = Auth()
         self.models = Models()
+        self.twofactor_auth = TwoFactor_Auth()
+
---
+++ resin/twofactor_auth.py
@@ -0,0 +1,63 @@
+from .base_request import BaseRequest
+from .token import Token
+from .settings import Settings
+from . import exceptions
+
+
+class TwoFactor_Auth(object):
+    """
+    This class implements basic 2FA functionalities for Resin Python SDK.
+
+    """
+
+
+    def __init__(self):
+        self.base_request = BaseRequest()
+        self.settings = Settings()
+        self.token = Token()
+
+    def is_enabled(self):
+        """
+        Check if two factor authentication is enabled.
+
+        Returns:
+            bool: True if enabled. Otherwise False.
+
+        """
+
+    	try:
+            self.token.get_property('twoFactorRequired')
+            return True
+    	except exceptions.InvalidOption:
+    		return False
+
+    def is_passed(self):
+        """
+        Check if two factor authentication challenge was passed.
+
+        Returns:
+            bool: True if enabled. Otherwise False.
+
+        """
+
+        if not self.is_enabled():
+            return True
+    	return not self.token.get_property('twoFactorRequired')
+
+    def challenge(self, token):
+        """
+        Challenge two factor authentication.
+
+        Args:
+            token (str): two factor authentication token.
+
+        """
+
+        data = {
+            'code': token
+        }
+        token = self.base_request.request(
+            'auth/totp/verify', 'POST', data=data,
+            endpoint=self.settings.get('api_endpoint'), login=True
+        )
+        self.token.set(token)
