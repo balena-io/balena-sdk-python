83589161bb6a29195650232830f1d86142b4a24d
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-04-26 18:44:11 +0700

    Using device_uuid instead of device_id in all supervisor functions for ease of use

--- resin/models/supervisor.py
+++ resin/models/supervisor.py
@@ -3,6 +3,7 @@ import os
 from ..base_request import BaseRequest
 from ..settings import Settings
 from .. import exceptions
+from .device import Device
 
 
 class Supervisor(object):
@@ -31,17 +32,25 @@ class Supervisor(object):
     def __init__(self):
         self.base_request = BaseRequest()
         self.settings = Settings()
+        self.device = Device()
+        self._last_device = None
 
-    def _check_args(self, device_id, app_id):
-        if device_id is None:
-            exceptions.MissingOption('device_id')
+    def _check_args(self, device_uuid, app_id):
+        if device_uuid is None:
+            exceptions.MissingOption('device_uuid')
 
         if app_id is None:
             exceptions.MissingOption('app_id')
 
-    def _do_command(self, endpoint, req_data={}, device_id=None, app_id=None, method=None):
+    def _do_command(self, endpoint, req_data={}, device_uuid=None, app_id=None, method=None):
         if not self._on_device:
-            self._check_args(device_id, app_id)
+            self._check_args(device_uuid, app_id)
+
+            if (self._last_device and self._last_device['uuid'] != device_uuid) or not self._last_device:
+                print('Get new!')
+                self._last_device = self.device.get(device_uuid)
+
+            device_id = self._last_device['id']
 
             if req_data:
                 data = {
@@ -85,29 +94,35 @@ class Supervisor(object):
 
         self._on_device = bool(endpoint)
 
-    def ping(self, device_id=None, app_id=None):
+    def ping(self, device_uuid=None, app_id=None):
         """
         Check that the supervisor is alive and well.
-        No need to set device_id and app_id if command is sent to the API on device.
+        No need to set device uuid and app_id if command is sent to the API on device.
 
         Args:
-            device_id (Optional[str]): device id.
+            device_uuid (Optional[str]): device uuid.
             app_id (Optional[str]): application id.
 
         Returns:
             str: `OK` signals that the supervisor is alive and well.
 
         Raises:
-            InvalidOption: if the endpoint is Resin API proxy endpoint and device_id or app_id is not set.
+            InvalidOption: if the endpoint is Resin API proxy endpoint and device_uuid or app_id is not set.
 
         Examples:
-            >>> resin.models.supervisor.ping(device_id='122950', app_id='9020')
+            >>> resin.models.supervisor.ping(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='9020')
             'OK'
 
         """
 
         if not self._on_device:
-            self._check_args(device_id, app_id)
+            self._check_args(device_uuid, app_id)
+
+            if (self._last_device and self._last_device['uuid'] != device_uuid) or not self._last_device:
+                print('Get new!')
+                self._last_device = self.device.get(device_uuid)
+
+            device_id = self._last_device['id']
 
             data = {
                 'deviceId': device_id,
@@ -130,50 +145,50 @@ class Supervisor(object):
                 api_key=self.RESIN_SUPERVISOR_API_KEY
             )
 
-    def blink(self, device_id=None, app_id=None):
+    def blink(self, device_uuid=None, app_id=None):
         """
         Start a blink pattern on a LED for 15 seconds. This is the same with `resin.models.device.identify()`.
-        No need to set device_id and app_id if command is sent to the API on device.
+        No need to set device_uuid and app_id if command is sent to the API on device.
 
         Args:
-            device_id (Optional[str]): device id.
+            device_uuid (Optional[str]): device uuid.
             app_id (Optional[str]): application id.
 
         Raises:
-            InvalidOption: if the endpoint is Resin API proxy endpoint and device_id or app_id is not specified.
+            InvalidOption: if the endpoint is Resin API proxy endpoint and device_uuid or app_id is not specified.
 
         Examples:
-            >>> resin.models.supervisor.blink(device_id='122950', app_id='9020')
+            >>> resin.models.supervisor.blink(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='9020')
             'OK'
 
         """
 
         return self._do_command(
             '{0}/blink'.format(self.SUPERVISOR_API_VERSION),
-            device_id=device_id,
+            device_uuid=device_uuid,
             app_id=app_id,
             method='POST'
         )
 
-    def update(self, device_id=None, app_id=None, force=False):
+    def update(self, device_uuid=None, app_id=None, force=False):
         """
         Triggers an update check on the supervisor. Optionally, forces an update when updates are locked.
-        No need to set device_id and app_id if command is sent to the API on device.
+        No need to set device_uuid and app_id if command is sent to the API on device.
 
         Args:
-            device_id (Optional[str]): device id.
+            device_uuid (Optional[str]): device uuid.
             app_id (Optional[str]): application id.
             force (Optional[bool]): If force is True, the update lock will be overridden.
 
         Raises:
-            InvalidOption: if the endpoint is Resin API proxy endpoint and device_id or app_id is not specified.
+            InvalidOption: if the endpoint is Resin API proxy endpoint and device_uuid or app_id is not specified.
 
         Examples:
-            >>> resin.models.supervisor.update(device_id='122950', app_id='9020')
+            >>> resin.models.supervisor.update(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='9020')
             (Empty Response)
 
             # Force an update
-            >>> resin.models.supervisor.update(device_id='122950', app_id='9020', force=True)
+            >>> resin.models.supervisor.update(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='9020', force=True)
             (Empty Response)
 
         """
@@ -188,84 +203,84 @@ class Supervisor(object):
         return self._do_command(
             '{0}/update'.format(self.SUPERVISOR_API_VERSION),
             req_data=data,
-            device_id=device_id,
+            device_uuid=device_uuid,
             app_id=app_id,
             method='POST'
         )
 
-    def reboot(self, device_id=None, app_id=None):
+    def reboot(self, device_uuid=None, app_id=None):
         """
         Reboot the device.
-        No need to set device_id and app_id if command is sent to the API on device.
+        No need to set device_uuid and app_id if command is sent to the API on device.
 
         Args:
-            device_id (Optional[str]): device id.
+            device_uuid (Optional[str]): device uuid.
             app_id (Optional[str]): application id.
 
         Returns:
             dict: when successful, this dictionary is returned `{ 'Data': 'OK', 'Error': '' }`.
 
         Raises:
-            InvalidOption: if the endpoint is Resin API proxy endpoint and device_id or app_id is not specified.
+            InvalidOption: if the endpoint is Resin API proxy endpoint and device_uuid or app_id is not specified.
 
         Examples:
-            >>> resin.models.supervisor.reboot(device_id='122950', app_id='9020')
+            >>> resin.models.supervisor.reboot(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='9020')
             {u'Data': u'OK', u'Error': u''}
 
         """
 
         return self._do_command(
             '{0}/reboot'.format(self.SUPERVISOR_API_VERSION),
-            device_id=device_id,
+            device_uuid=device_uuid,
             app_id=app_id,
             method='POST'
         )
 
-    def shutdown(self, device_id=None, app_id=None):
+    def shutdown(self, device_uuid=None, app_id=None):
         """
         Shut down the device.
-        No need to set device_id and app_id if command is sent to the API on device.
+        No need to set device_uuid and app_id if command is sent to the API on device.
 
         Args:
-            device_id (Optional[str]): device id.
+            device_uuid (Optional[str]): device uuid.
             app_id (Optional[str]): application id.
 
         Returns:
             dict: when successful, this dictionary is returned `{ 'Data': 'OK', 'Error': '' }`.
 
         Raises:
-            InvalidOption: if the endpoint is Resin API proxy endpoint and device_id or app_id is not specified.
+            InvalidOption: if the endpoint is Resin API proxy endpoint and device_uuid or app_id is not specified.
 
         Examples:
-            >>> resin.models.supervisor.shutdown(device_id='121867', app_id='8362')
+            >>> resin.models.supervisor.shutdown(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='8362')
             {u'Data': u'OK', u'Error': u''}
 
         """
 
         return self._do_command(
             '{0}/shutdown'.format(self.SUPERVISOR_API_VERSION),
-            device_id=device_id,
+            device_uuid=device_uuid,
             app_id=app_id,
             method='POST'
         )
 
-    def purge(self, app_id, device_id=None):
+    def purge(self, app_id, device_uuid=None):
         """
         Clears the user application's /data folder.
-        No need to set device_id and app_id if command is sent to the API on device.
+        No need to set device_uuid and app_id if command is sent to the API on device.
 
         Args:
             app_id (str): application id.
-            device_id (Optional[str]): device id.
+            device_uuid (Optional[str]): device uuid.
 
         Returns:
             dict: when successful, this dictionary is returned `{ 'Data': 'OK', 'Error': '' }`.
 
         Raises:
-            InvalidOption: if the endpoint is Resin API proxy endpoint and device_id or app_id is not specified.
+            InvalidOption: if the endpoint is Resin API proxy endpoint and device_uuid or app_id is not specified.
 
         Examples:
-            >>> resin.models.supervisor.purge(device_id='122950', app_id='9020')
+            >>> resin.models.supervisor.purge(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='9020')
             {u'Data': u'OK', u'Error': u''}
 
         """
@@ -277,28 +292,28 @@ class Supervisor(object):
         return self._do_command(
             '{0}/purge'.format(self.SUPERVISOR_API_VERSION),
             req_data=data,
-            device_id=device_id,
+            device_uuid=device_uuid,
             app_id=app_id,
             method='POST'
         )
 
-    def restart(self, app_id, device_id=None):
+    def restart(self, app_id, device_uuid=None):
         """
         Restart user application container.
-        No need to set device_id and app_id if command is sent to the API on device.
+        No need to set device_uuid and app_id if command is sent to the API on device.
 
         Args:
             app_id (str): application id.
-            device_id (Optional[str]): device id.
+            device_uuid (Optional[str]): device uuid.
 
         Returns:
             str: `OK` signals that the supervisor is alive and well.
 
         Raises:
-            InvalidOption: if the endpoint is Resin API proxy endpoint and device_id or app_id is not specified.
+            InvalidOption: if the endpoint is Resin API proxy endpoint and device_uuid or app_id is not specified.
 
         Examples:
-            >>> resin.models.supervisor.restart(device_id='122950', app_id='9020')
+            >>> resin.models.supervisor.restart(device_uuid='8f66ec7335267e7cc7999ca9eec029a01ea7d823214c742ace5cfffaa21be3', app_id='9020')
             'OK'
 
         """
@@ -310,7 +325,7 @@ class Supervisor(object):
         return self._do_command(
             '{0}/restart'.format(self.SUPERVISOR_API_VERSION),
             req_data=data,
-            device_id=device_id,
+            device_uuid=device_uuid,
             app_id=app_id,
             method='POST'
         )
