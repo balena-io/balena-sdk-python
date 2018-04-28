925c6ea160493729e93a25113d40057b06b07838
Trong Nghia Nguyen <nghiant2710@gmail.com>
2018-04-28 10:52:59 +0700

    Re-add resin.models.environment_variables.DeviceEnvVariable model.
    Change-Type: minor

--- resin/models/environment_variables.py
+++ resin/models/environment_variables.py
@@ -21,7 +21,162 @@ class EnvironmentVariable(object):
 
     def __init__(self):
         self.service_environment_variable = ServiceEnvVariable()
+        self.device = DeviceEnvVariable()
         self.device_service_environment_variable = DeviceServiceEnvVariable()
+
+
+class DeviceEnvVariable(object):
+    """
+    This class implements device environment variable model for Resin Python SDK.
+
+    """
+
+    def __init__(self):
+        self.base_request = BaseRequest()
+        self.device = Device()
+        self.settings = Settings()
+
+    def _fix_device_env_var_name_key(self, env_var):
+        """
+        Internal method to workaround the fact that applications environment variables contain a `name` property
+        while device environment variables contain an `env_var_name` property instead.
+        """
+
+        if 'env_var_name' in env_var:
+            env_var['name'] = env_var['env_var_name']
+            env_var.pop('env_var_name', None)
+        return env_var
+
+    def get_all(self, uuid):
+        """
+        Get all device environment variables.
+
+        Args:
+            uuid (str): device uuid.
+
+        Returns:
+            list: device environment variables.
+
+        Examples:
+            >>> resin.models.environment_variables.device.get_all('8deb12a58e3b6d3920db1c2b6303d1ff32f23d5ab99781ce1dde6876e8d143')
+            [{u'device': {u'__deferred': {u'uri': u'/ewa/device(122950)'}, u'__id': 122950}, u'__metadata': {u'type': u'', u'uri': u'/ewa/device_environment_variable(2173)'}, u'id': 2173, u'value': u'1322944771964103', u'env_var_name': u'RESIN_DEVICE_RESTART'}]
+
+        """
+
+        device = self.device.get(uuid)
+        params = {
+            'filter': 'device',
+            'eq': device['id']
+        }
+        return self.base_request.request(
+            'device_environment_variable', 'GET', params=params,
+            endpoint=self.settings.get('pine_endpoint')
+        )['d']
+
+    def create(self, uuid, env_var_name, value):
+        """
+        Create a device environment variable.
+
+        Args:
+            uuid (str): device uuid.
+            env_var_name (str): environment variable name.
+            value (str): environment variable value.
+
+        Returns:
+            dict: new device environment variable info.
+
+        Examples:
+            >>> resin.models.environment_variables.device.create('8deb12a58e3b6d3920db1c2b6303d1ff32f23d5ab99781ce1dde6876e8d143','test_env4', 'testing1')
+            {'name': u'test_env4', u'__metadata': {u'type': u'', u'uri': u'/resin/device_environment_variable(42166)'}, u'value': u'testing1', u'device': {u'__deferred': {u'uri': u'/resin/device(115792)'}, u'__id': 115792}, u'id': 42166}
+
+        """
+
+        if not _is_valid_env_var_name(env_var_name):
+            raise exceptions.InvalidParameter('env_var_name', env_var_name)
+        device = self.device.get(uuid)
+        data = {
+            'device': device['id'],
+            'env_var_name': env_var_name,
+            'value': value
+        }
+        new_env_var = json.loads(self.base_request.request(
+            'device_environment_variable', 'POST', data=data,
+            endpoint=self.settings.get('pine_endpoint')
+        ).decode('utf-8'))
+        return self._fix_device_env_var_name_key(new_env_var)
+
+    def update(self, var_id, value):
+        """
+        Update a device environment variable.
+
+        Args:
+            var_id (str): environment variable id.
+            value (str): new environment variable value.
+
+        Examples:
+            >>> resin.models.environment_variables.device.update(2184, 'new value')
+            'OK'
+
+        """
+
+        params = {
+            'filter': 'id',
+            'eq': var_id
+        }
+        data = {
+            'value': value
+        }
+        return self.base_request.request(
+            'device_environment_variable', 'PATCH', params=params, data=data,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+    def remove(self, var_id):
+        """
+        Remove a device environment variable.
+
+        Args:
+            var_id (str): environment variable id.
+
+        Examples:
+            >>> resin.models.environment_variables.device.remove(2184)
+            'OK'
+
+        """
+
+        params = {
+            'filter': 'id',
+            'eq': var_id
+        }
+        return self.base_request.request(
+            'device_environment_variable', 'DELETE', params=params,
+            endpoint=self.settings.get('pine_endpoint')
+        )
+
+    def get_all_by_application(self, app_id):
+        """
+        Get all device environment variables for an application.
+
+        Args:
+            app_id (str): application id.
+
+        Returns:
+            list: list of device environment variables.
+
+        Examples:
+            >>> resin.models.environment_variables.device.get_all_by_application('5780')
+            [{'name': u'device1', u'__metadata': {u'type': u'', u'uri': u'/resin/device_environment_variable(40794)'}, u'value': u'test', u'device': {u'__deferred': {u'uri': u'/resin/device(115792)'}, u'__id': 115792}, u'id': 40794}, {'name': u'RESIN_DEVICE_RESTART', u'__metadata': {u'type': u'', u'uri': u'/resin/device_environment_variable(1524)'}, u'value': u'961506585823372', u'device': {u'__deferred': {u'uri': u'/resin/device(121794)'}, u'__id': 121794}, u'id': 1524}]
+
+        """
+
+        params = {
+            'filter': 'device/belongs_to__application',
+            'eq': app_id
+        }
+        env_list = self.base_request.request(
+            'device_environment_variable', 'GET', params=params,
+            endpoint=self.settings.get('pine_endpoint'))
+        return list(map(self._fix_device_env_var_name_key, env_list['d']))
 
 
 class DeviceServiceEnvVariable(object):
